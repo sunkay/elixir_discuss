32a008098dc2f568b718206624937c6d72b66e8f
sunkay <sunkay11@gmail.com>
2016-11-06 17:55:31 -0500

    CRUD in phoenix/elixir

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,31 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        IO.inspect changeset
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+end
